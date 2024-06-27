422c3a354113f34e07578ada6b0328469055bd8d
Gloal <alori.glo@gmail.com>
2024-06-27 17:15:35 +0100

    fix:Changed button text to buy

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -212,7 +212,7 @@ export default function Form({ setSubmittedData }) {
         type="submit"
         className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       >
-        Submit
+        Buy
       </button>
     </form>
   );
